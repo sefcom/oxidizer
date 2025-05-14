81eab93cd0028f02f46f8f37f1f580d10d02942b
bluesadi <yiboliume@gmail.com>
2025-05-14 14:02:16 -0700

    Fix some bugs related to PatternMatchSimplifier and typeconsts

--- angr/analyses/typehoon/typeconsts.py
+++ angr/analyses/typehoon/typeconsts.py
@@ -53,11 +53,15 @@ class TypeConstant:
 
 
 class TopType(TypeConstant):
+    SIZE = 1
+
     def __repr__(self, memo=None):
         return "TOP"
 
 
 class BottomType(TypeConstant):
+    SIZE = 1
+
     def __repr__(self, memo=None):
         return "BOT"
 
